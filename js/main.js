b017e6c7eaae5febca724199aa69db31dcad1663
Astrid Vilora <astridviloria54@gmail.com>
2024-04-23 22:58:54 +0200

    Se realiza un cambio en css para adaptar la imagen y se crea el mapeo de los canales y la funcion para cambiar de canal

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-// Se crea una simulación de un televisor con funcionalidades básicas como encender y apagar, cambiar de canal, mostrar información sobre el canal actual y mostrar la fecha y hora. 
-
+// Se crea una simulación de un televisor con funcionalidades básicas como encender y apagar, cambiar de canal, mostrar información sobre el canal actual y mostrar la fecha y hora.
 
 // 1- Se realiza la declaración de variables para los elementos del DOM que se utilizara
 
@@ -7,24 +6,23 @@ let onOff = document.getElementById("btnOnOff");
 let sizeTv = document.getElementById("sizeTv");
 let infoDate = document.getElementById("dateHour");
 let infoChannel = document.getElementById("channelName");
-let tvStatus = false;   //Estado del televisor es false para indicar que esta apagado inicialmente.
+let tvStatus = false; //Estado del televisor es false para indicar que esta apagado inicialmente.
 
-
-// Encender y apagar el televisor: Se controla el encendido y apagado de la TV, cambia la imagen de fondo del televisor para reflejar su estado y manejar la visualización de la información relacionada con la fecha y el canal dependiendo de si la TV está encendida o apagada. 
+// Encender y apagar el televisor: Se controla el encendido y apagado de la TV, cambia la imagen de fondo del televisor para reflejar su estado y manejar la visualización de la información relacionada con la fecha y el canal dependiendo de si la TV está encendida o apagada.
 
 onOff.addEventListener("click", () => {
-    tvStatus = !tvStatus; 
-    console.log(tvStatus);
-    infoDate.style.display = "none";
-  
-    if (tvStatus) {
-      sizeTv.style.backgroundImage = 'url("../img/pantalla.jpg")';
-    } else {
-      sizeTv.style.backgroundImage = "none";
-      hideInfoChannel();
-      hideInfoDate();
-    }
-  });
+  tvStatus = !tvStatus;
+  console.log(tvStatus);
+  infoDate.style.display = "none";
+
+  if (tvStatus) {
+    sizeTv.style.backgroundImage = 'url("../img/pantalla.jpg")';
+  } else {
+    sizeTv.style.backgroundImage = "none";
+    hideInfoChannel();
+    hideInfoDate();
+  }
+});
 
 // Sección de canales:Se seleccionan todos los botones de canal y se agrega un evento de clic a cada uno.
 // Al hacer clic en un canal, se muestra su nombre y se cambia la imagen del televisor al canal seleccionado.
@@ -33,18 +31,18 @@ const arrayMandoInf = document.querySelectorAll(".mandoInf .chanBtn");
 arrayMandoInf.forEach((channel) => {
   channel.addEventListener("click", (e) => {
     const channelName = e.target.id;
-    selectedChannel = channelName; 
+    selectedChannel = channelName;
     infoDate.style.display = "inline-block";
     infoChannel.style.display = "inline-block";
 
-    document.getElementById("channelName").innerHTML = channelName; 
+    document.getElementById("channelName").innerHTML = channelName;
 
     setTimeout(hideInfoDate, 3000);
     setTimeout(hideInfoChannel, 3000);
 
     if (tvStatus) {
       showInfo(Date);
-      changeChannelImage(channelName); 
+      changeChannelImage(channelName);
     } else {
       infoDate.style.display = "none";
       infoChannel.style.display = "none";
@@ -52,19 +50,62 @@ arrayMandoInf.forEach((channel) => {
   });
 });
 
-
 // Función para fecha y hora:Se utiliza función showInfo() que va a encarga de mostrar la fecha y la hora actual en un elemento del DOM.
 function showInfo() {
-    let hour = document.getElementById("dateHour");
-    setInterval(function () {
-      let data = new Date();
-      return (hour.innerHTML =
-        data.getDate() +
-        "/" +
-        (data.getMonth() + 1) +  // se usa +1 para obtener el número de mes en formato de 1 a 12 en lugar de 0 a 11.
-        "/" +
-        data.getFullYear() +
-        "  " +
-        data.toLocaleTimeString());
-    });
+  let hour = document.getElementById("dateHour");
+  setInterval(function () {
+    let data = new Date();
+    return (hour.innerHTML =
+      data.getDate() +
+      "/" +
+      (data.getMonth() + 1) + // se usa +1 para obtener el número de mes en formato de 1 a 12 en lugar de 0 a 11.
+      "/" +
+      data.getFullYear() +
+      "  " +
+      data.toLocaleTimeString());
+  });
+}
+
+//   Función para ocultar información
+
+// Función para ocultar la información de fecha y hora después de 3 segundos
+function hideInfoDate() {
+  infoDate.style.display = "none";
+}
+
+// Función para ocultar el nombre del canal después de 3 segundos
+function hideInfoChannel() {
+  infoChannel.style.display = "none";
+}
+
+//Mapeo con los nombres de los canales y las Url de las imagenes
+const channelImages = {
+  "Noticias24": "../img/noti.gif",
+  "Fox Sport": "../img/2.gif",
+  "Antena3": "../img/pasa.gif",
+  "Cinemax": "../img/peli.gif",
+  "Atresplayer": "../img/tele5.gif",
+  "LaSexta": "../img/6.gif",
+  "Gol t": "../img/7.gif",
+  "Divinity": "../img/8.gif",
+  "Telemundo": "../img/9.gif",
+  "channelInfo": "../img/pgm.png",
+  "Sin Signal": "../img/0.gif",
+  "netflix": "../img/netflix.gif",
+  "disney": "../img/disney.gif",
+  "prime": "../img/primeV.gif",
+  "Pluto": "../img/movi.gif",
+};
+
+
+// Función para cambiar la imagen del televisor según el nombre del canal: la función se va a encarga de cambiar la imagen de fondo de la pantalla de la televisión según el nombre del canal proporcionado. Si se encuentra una imagen asociada al canal, se muestra en la pantalla; de lo contrario, se elimina cualquier imagen de fondo existente.
+function changeChannelImage(channelName) {
+    const imageUrl = channelImages[channelName];
+    if (imageUrl) {
+      sizeTv.style.backgroundImage = `url(${imageUrl})`;
+    } else {
+      // Si no se encuentra una imagen para el canal, se elimina la imagen de fondo
+      sizeTv.style.backgroundImage = "none";
+    }
   }
+
